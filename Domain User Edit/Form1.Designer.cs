40d13685ba277f164b602427f5b8d702d4ed0608
Neophear <stiiggade@gmail.com>
2019-10-11 09:42:12 +0200

    Initial commit

---
+++ Domain User Edit/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Domain_User_Edit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnGetUser = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.chkbxUnlock = new System.Windows.Forms.CheckBox();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.lblChangePassword = new System.Windows.Forms.Label();
+            this.lblChangePassword2 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblIsLocked = new System.Windows.Forms.Label();
+            this.lnklblHelp = new System.Windows.Forms.LinkLabel();
+            this.ssMain = new System.Windows.Forms.StatusStrip();
+            this.tssMain = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ssMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 12);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // btnGetUser
+            // 
+            this.btnGetUser.Location = new System.Drawing.Point(118, 10);
+            this.btnGetUser.Name = "btnGetUser";
+            this.btnGetUser.Size = new System.Drawing.Size(75, 23);
+            this.btnGetUser.TabIndex = 1;
+            this.btnGetUser.Text = "Get user";
+            this.btnGetUser.UseVisualStyleBackColor = true;
+            this.btnGetUser.Click += new System.EventHandler(this.btnGetUser_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(12, 35);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(65, 13);
+            this.lblUser.TabIndex = 10;
+            this.lblUser.Text = "lblUsername";
+            // 
+            // chkbxUnlock
+            // 
+            this.chkbxUnlock.AutoSize = true;
+            this.chkbxUnlock.Checked = true;
+            this.chkbxUnlock.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkbxUnlock.Enabled = false;
+            this.chkbxUnlock.Location = new System.Drawing.Point(12, 51);
+            this.chkbxUnlock.Name = "chkbxUnlock";
+            this.chkbxUnlock.Size = new System.Drawing.Size(83, 17);
+            this.chkbxUnlock.TabIndex = 2;
+            this.chkbxUnlock.Text = "Unlock user";
+            this.chkbxUnlock.UseVisualStyleBackColor = true;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Enabled = false;
+            this.txtNewPassword.Location = new System.Drawing.Point(12, 74);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtNewPassword.TabIndex = 4;
+            // 
+            // lblChangePassword
+            // 
+            this.lblChangePassword.AutoSize = true;
+            this.lblChangePassword.Location = new System.Drawing.Point(118, 74);
+            this.lblChangePassword.Name = "lblChangePassword";
+            this.lblChangePassword.Size = new System.Drawing.Size(77, 13);
+            this.lblChangePassword.TabIndex = 5;
+            this.lblChangePassword.Text = "New password";
+            // 
+            // lblChangePassword2
+            // 
+            this.lblChangePassword2.AutoSize = true;
+            this.lblChangePassword2.Location = new System.Drawing.Point(118, 87);
+            this.lblChangePassword2.Name = "lblChangePassword2";
+            this.lblChangePassword2.Size = new System.Drawing.Size(137, 13);
+            this.lblChangePassword2.TabIndex = 6;
+            this.lblChangePassword2.Text = "(leave blank to not change)";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.Location = new System.Drawing.Point(12, 100);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(83, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Run changes";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblIsLocked
+            // 
+            this.lblIsLocked.AutoSize = true;
+            this.lblIsLocked.Location = new System.Drawing.Point(118, 52);
+            this.lblIsLocked.Name = "lblIsLocked";
+            this.lblIsLocked.Size = new System.Drawing.Size(61, 13);
+            this.lblIsLocked.TabIndex = 9;
+            this.lblIsLocked.Text = "lblIsLocked";
+            // 
+            // lnklblHelp
+            // 
+            this.lnklblHelp.AutoSize = true;
+            this.lnklblHelp.LinkColor = System.Drawing.Color.Black;
+            this.lnklblHelp.Location = new System.Drawing.Point(242, 9);
+            this.lnklblHelp.Name = "lnklblHelp";
+            this.lnklblHelp.Size = new System.Drawing.Size(13, 13);
+            this.lnklblHelp.TabIndex = 10;
+            this.lnklblHelp.TabStop = true;
+            this.lnklblHelp.Text = "?";
+            this.lnklblHelp.Click += new System.EventHandler(this.lnklblHelp_Click);
+            // 
+            // ssMain
+            // 
+            this.ssMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssMain});
+            this.ssMain.Location = new System.Drawing.Point(0, 129);
+            this.ssMain.Name = "ssMain";
+            this.ssMain.Size = new System.Drawing.Size(259, 22);
+            this.ssMain.SizingGrip = false;
+            this.ssMain.TabIndex = 12;
+            this.ssMain.Text = "statusStrip1";
+            // 
+            // tssMain
+            // 
+            this.tssMain.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tssMain.Name = "tssMain";
+            this.tssMain.Size = new System.Drawing.Size(48, 17);
+            this.tssMain.Text = "tssMain";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 151);
+            this.Controls.Add(this.ssMain);
+            this.Controls.Add(this.lnklblHelp);
+            this.Controls.Add(this.lblIsLocked);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblChangePassword2);
+            this.Controls.Add(this.lblChangePassword);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.chkbxUnlock);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.btnGetUser);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Domain User Edit";
+            this.ssMain.ResumeLayout(false);
+            this.ssMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnGetUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.CheckBox chkbxUnlock;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.Label lblChangePassword;
+        private System.Windows.Forms.Label lblChangePassword2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblIsLocked;
+        private System.Windows.Forms.LinkLabel lnklblHelp;
+        private System.Windows.Forms.StatusStrip ssMain;
+        private System.Windows.Forms.ToolStripStatusLabel tssMain;
+    }
+}
+
---
+++ Domain User Edit/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Domain_User_Edit
+{
+    public partial class Form1 : Form
+    {
+        private string LDAPDomainName = string.Empty;
+        private string username = string.Empty;
+        private DirectoryEntry user;
+        private string bottomMessage = string.Empty;
+        private string fullname = String.Empty;
+        //IntPtr accessToken = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Tools.Network.Impersonator impersonate = new Tools.Network.Impersonator();
+            //impersonate.Impersonate("diablo666", "INTERNET.LOCAL", "diablo666**", Tools.Network.LogonType.LOGON32_LOGON_NEW_CREDENTIALS, Tools.Network.LogonProvider.LOGON32_PROVIDER_WINNT50);
+
+            if (!CheckIfInDomain())
+            {
+                MessageBox.Show("You are not part of a domain.\nThis application will now terminate.");
+                Environment.Exit(1);
+            }
+
+            LDAPDomainName = GetDomainDN(IPGlobalProperties.GetIPGlobalProperties().DomainName);
+
+            lblUser.Text = string.Empty;
+            lblIsLocked.Text = string.Empty;
+            tssMain.Text = String.Empty;
+        }
+
+        public bool CheckIfInDomain()
+        {
+            bool isInDomain = true;
+
+            try
+            {
+                Domain.GetComputerDomain();
+            }
+            catch (ActiveDirectoryObjectNotFoundException)
+            {
+                isInDomain = false;
+            }
+
+            return isInDomain;
+        }
+
+        private string GetDomainDN(string domain)
+        {
+            DirectoryContext context = new DirectoryContext(DirectoryContextType.Domain, domain);
+            Domain d = Domain.GetDomain(context);
+            DirectoryEntry de = d.GetDirectoryEntry();
+            return de.Properties["DistinguishedName"].Value.ToString();
+        }
+
+        private void btnGetUser_Click(object sender, EventArgs e)
+        {
+            GetUser();
+        }
+
+        private void GetUser()
+        {
+            tssMain.Text = string.Empty;
+
+            if (UserExists(txtUsername.Text))
+            {
+                username = txtUsername.Text;
+
+                user = GetUser(username);
+
+                fullname = String.Format("{0} {1}", user.Properties["givenName"].Value.ToString(), user.Properties["sn"].Value.ToString());
+                lblUser.Text = fullname;
+                chkbxUnlock.Enabled = true;
+                txtNewPassword.Enabled = true;
+                btnReset.Enabled = true;
+                txtNewPassword.Focus();
+                IsUserLocked();
+            }
+            else
+            {
+                lblUser.Text = string.Empty;
+                SetMessage("User doesn't exist.");
+                chkbxUnlock.Enabled = false;
+                txtNewPassword.Enabled = false;
+                txtNewPassword.Text = string.Empty;
+                btnReset.Enabled = false;
+                lblIsLocked.Text = string.Empty;
+            }
+        }
+
+        public DirectoryEntry GetUser(string userName)
+        {
+            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://" + LDAPDomainName);
+            DirectorySearcher directorySearcher = new DirectorySearcher(directoryEntry);
+            directorySearcher.Filter = "(&(objectCategory=person)(sAMAccountName=" + userName + "))";
+            DirectoryEntry result = directorySearcher.FindOne().GetDirectoryEntry();
+            return result;
+        }
+
+        public static bool UserExists(string userName)
+        {
+            using (var pc = new PrincipalContext(ContextType.Domain))
+            using (var p = Principal.FindByIdentity(pc, IdentityType.SamAccountName, userName))
+            {
+                return p != null;
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ChangeUser();
+        }
+        private void SetMessage(string message)
+        {
+            tssMain.Text = message;
+        }
+        private void ChangeUser()
+        {
+            tssMain.Text = string.Empty;
+            bool error = false;
+
+            if (chkbxUnlock.Checked)
+                user.Properties["LockOutTime"].Value = 0;
+            
+            if (txtNewPassword.Text != string.Empty)
+                error = ChangePassword();
+
+            try
+            {
+                user.CommitChanges();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("You do not have sufficient permissions to do this");
+                this.Close();
+                Application.Exit();
+                throw;
+            }
+
+            IsUserLocked();
+
+            if (!error)
+            {
+                txtUsername.Focus();
+                txtUsername.SelectAll();
+            }
+        }
+
+        private bool ChangePassword()
+        {
+            int counter = 0;
+            bool error = true;
+            bool nameError = false;
+            string newPassword = txtNewPassword.Text;
+
+            Regex hasCapitalLetter = new Regex("[A-Z]");
+            Regex hasSmallLetter = new Regex("[a-z]");
+            Regex hasNumber = new Regex("[0-9]");
+            Regex hasSpecialChar = new Regex(@"[\W]");
+
+            if (hasCapitalLetter.IsMatch(newPassword)) { counter++; }
+            if (hasSmallLetter.IsMatch(newPassword)) { counter++; }
+            if (hasNumber.IsMatch(newPassword)) { counter++; }
+            if (hasSpecialChar.IsMatch(newPassword)) { counter++; }
+
+            foreach (string part in fullname.Split(' '))
+            {
+                if (newPassword.ToLower().Contains(part.ToLower()))
+                    nameError = true;
+            }
+
+            if (newPassword.Length < 8)
+                SetMessage("Password must be at least 8 characters");
+            else if (nameError)
+                SetMessage("Password can't contain parts of the name");
+            else
+            {
+                if (counter >= 3)
+                {
+                    user.Invoke("SetPassword", new object[] { newPassword });
+                    user.Properties["pwdLastSet"].Value = 0;
+
+                    SetMessage("Password changed");
+
+                    error = false;
+                }
+                else
+                    SetMessage("The password doesn't meet the policy");
+            }
+
+            return error;
+        }
+
+        public void IsUserLocked()
+        {
+            lblIsLocked.Text = Convert.ToBoolean(user.InvokeGet("IsAccountLocked")) ? "Locked" : "Unlocked";
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+
+            if (keyData == Keys.Enter)
+            {
+                if (txtUsername.Focused)
+                    GetUser();
+                else if (txtNewPassword.Focused)
+                    ChangeUser();
+
+                return true;
+            }
+
+            if (keyData == Keys.F1)
+                ShowHelp();
+
+            if (keyData == Keys.F2)
+            {
+                txtUsername.Focus();
+                txtUsername.SelectAll();
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void ShowHelp()
+        {
+            MessageBox.Show("Made by Stiig Gade\nF1 shows help (this)\nF2 goes to username-box\nEsc closes\nVersion " + ProductVersion);
+        }
+
+        private void lnklblHelp_Click(object sender, EventArgs e)
+        {
+            ShowHelp();
+        }
+    }
+}
